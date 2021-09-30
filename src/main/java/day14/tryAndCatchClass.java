787165a28983b2ab74b2cb2f63c4ecf0a5c1174d
DevelopJKong <ybaram40@gmail.com>
2021-10-01 08:45:43 +0900

    day14 완료

--- src/main/java/day14/tryAndCatchClass.java
+++ src/main/java/day14/tryAndCatchClass.java
@@ -1,4 +1,22 @@
 package day14;
 
+import java.util.Scanner;
+
 public class tryAndCatchClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] value = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        while (true) {
+            System.out.print("please type number:");
+            int i = sc.nextInt();
+                try {
+                    System.out.println(value[i]);
+                } catch (Exception e){
+                    System.out.println("it's done");
+                    break;
+                }
+
+        }
+    }
 }
