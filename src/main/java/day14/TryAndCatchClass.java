3d290dc3ed537241a935d539dcb6c45b8b5f956b
DevelopJKong <ybaram40@gmail.com>
2021-10-01 08:50:54 +0900

    클래스 이름 수정

--- src/main/java/day14/TryAndCatchClass.java
+++ src/main/java/day14/TryAndCatchClass.java
@@ -2,7 +2,7 @@ package day14;
 
 import java.util.Scanner;
 
-public class tryAndCatchClass {
+public class TryAndCatchClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] value = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
